26582a0c1960f9e06340020c0a1df3a5569eb8e3
Paul Kim <khyunwoo1@gmail.com>
2020-08-24 20:59:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string):
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
+  }
     
-  if string.toUpperCase() === string:
+  elif (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  
   
 }
